a292f622d0acd47054675626a1e4c7d2beeb6a56
Evan Shimoniak <FracturedRetina>
2017-03-05 19:03:21 -0800

    Added language support

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var lang = getParameterByName("lang");
+
+if (lang == "") {
+	lang = "en"
+}
+
 $(document).ready(function() {
 	$.getJSON("bubbles.json", function(json) {
 		for (var i = 0; i < json.bubbles.length; i++) {
@@ -7,5 +13,12 @@ $(document).ready(function() {
 });
 
 function renderBubble(b) {
-	$("<div class=\"bubble\"><div class=\"img\" style=\"background-image: url(" + b.img + ")\"></div><h" + b.size + " class=\"title\">" + b.title.en + "</h" + b.size + "><p>" + b.description.en + "</p></div>").appendTo("#content");
+	$("<div class=\"bubble\"><div class=\"img\" style=\"background-image: url(" + b.img + ")\"></div><h" + b.size + " class=\"title\">" + b.title[lang] + "</h" + b.size + "><p>" + b.description[lang] + "</p></div>").appendTo("#content");
+}
+
+function getParameterByName(name) {
+	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+	results = regex.exec(location.search);
+	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
